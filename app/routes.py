0c41e4db5afc4019cc68c591d3b00a2294b02c74
Vesubius <Jeffryperezduarte@gmail.com>
2024-05-12 10:05:43 -0600

    Vista de Mapa

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,12 @@
 from app import app
 
+
+# Pagina principal 
 @app.route('/')
 def index():
     return "¡Hola, mundo!"
+
+
+@app.route("/Mapa")
+def Mapa():
+    return "En esta Vista Debe verse el mapa"
